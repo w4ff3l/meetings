9eda8a3265e86c6e5f022277141a2a2eff4237e3
Kevin Schmauß <kevin@schmauss.net>
2023-03-06 07:40:47 +0100

    update spring; add mongodb dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.3"
+    id("org.springframework.boot") version "3.0.4"
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
@@ -22,13 +22,19 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
-    implementation("com.discord4j:discord4j-core:3.2.3")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+
+    // Misc
+    implementation("com.discord4j:discord4j-core:3.2.3")
 
     compileOnly("org.projectlombok:lombok")
 
